54b182f0c84c52bd84677637d8cde43eb33136c7
Ferhat DİNÇ <ferhatdinc@github.com>
2019-12-25 23:03:15 +0300

    Update app-routing.module.ts

--- bubbles/src/app/app-routing.module.ts
+++ bubbles/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BubblesComponent } from './bubbles/bubbles.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { GenderChartComponent } from './gender-chart/gender-chart.component';
+import { OsChartComponent } from './os-chart/os-chart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "index", component: BubblesComponent},
+  {path: "signin", component: SignInComponent},
+  {path: "chart", component: GenderChartComponent},
+  {path: "deviceChart", component: OsChartComponent},
+  {path: "**", redirectTo: "index", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
